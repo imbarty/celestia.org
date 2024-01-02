e08926323e84ecc21d77f9ee852c9a2d276679e0
Alex Beckett <75361908+alex-beckett@users.noreply.github.com>
2024-01-02 23:37:10 +1030

    Update ecosystems.js

--- src/datas/ecosystem/ecosystems.js
+++ src/datas/ecosystem/ecosystems.js
@@ -1,260 +1,428 @@
 // 1. Define ecosystem items
 export const ecosystemItems = [
 	{
-		title: "Rollkit",
-		image: "ecosystem/rollkit.png",
-		url: "https://www.rollkit.dev/",
-	},
-	{
-		title: "Sovereign",
-		image: "ecosystem/sovereign.png",
-		url: "https://sovereign.xyz/",
-	},
-	{
-		title: "OP Stack",
-		image: "ecosystem/optimism.png",
-		url: "https://docs.optimism.io/stack/getting-started",
-	},
-	{
-		title: "Stackr",
-		image: "ecosystem/stackr.png",
-		url: "https://www.stackrlabs.xyz/",
-	},
-	{
-		title: "Cosmos SDK",
-		image: "ecosystem/cosmos-sdk.png",
-		url: "https://cosmos.network/",
-	},
-	{
-		title: "Arbitrum Orbit",
-		image: "ecosystem/arbitrum.png",
-		url: "https://arbitrum.io/orbit",
-	},
-	{
-		title: "Dymension",
-		image: "ecosystem/dymension.png",
-		url: "https://dymension.xyz/",
-	},
-	{
-		title: "Polygon CDK",
-		image: "ecosystem/polygoncdk.png",
-		url: "https://polygon.technology/polygon-cdk",
-	},
-	{
-		title: "Caldera",
-		image: "ecosystem/caldera.png",
-		url: "https://www.caldera.xyz/",
-	},
-	{
-		title: "Vistara",
-		image: "ecosystem/vistara.png",
-		url: "https://docs.vistara.dev/",
-	},
-	{
-		title: "Snapchain",
-		image: "ecosystem/snapchain.png",
-		url: "https://www.snapchain.dev/",
-	},
-	{
-		title: "Altlayer",
-		image: "ecosystem/altlayer.png",
-		url: "https://www.altlayer.io/",
-	},
-	{
 		title: "Astria",
 		image: "ecosystem/astria.png",
 		url: "https://astria.org/",
 	},
 	{
-		title: "Conduit",
-		image: "ecosystem/conduit.png",
-		url: "https://conduit.xyz/",
+		title: "Alpha Dune",
+		image: "ecosystem/alphadune.png",
+		url: "https://alphadune.com/",
 	},
 	{
-		title: "Gateway",
-		image: "ecosystem/gateway.png",
-		url: "https://gateway.fm/",
-	},
-	{
-		title: "Gelato",
-		image: "ecosystem/gelato.png",
-		url: "https://www.gelato.network/",
-	},
-	{
-		title: "Lumoz",
-		image: "ecosystem/lumoz.png",
-		url: "https://lumoz.org/",
-	},
-	{
-		title: "Zeeve",
-		image: "ecosystem/zeeve.png",
-		url: "https://www.zeeve.io/",
-	},
-	{
-		title: "Radius",
-		image: "ecosystem/radius.png",
-		url: "https://www.theradius.xyz/",
-	},
-	{
-		title: "Fairblock",
-		image: "ecosystem/fairblock.png",
-		url: "https://twitter.com/Fair_Block",
-	},
-	{
-		title: "Neutron",
-		image: "ecosystem/neutron.png",
-		url: "https://neutron.org/",
-	},
-	{
-		title: "Berachain",
-		image: "ecosystem/berachain.png",
-		url: "https://berachain.com/",
-	},
-	{
-		title: "Saga",
-		image: "ecosystem/saga.png",
-		url: "https://www.saga.xyz/",
-	},
-	{
-		title: "Polaris",
-		image: "ecosystem/berachain.png",
-		url: "https://berachain.com/",
-	},
-	{
-		title: "CosmWasm",
-		image: "ecosystem/cosmwasm.png",
-		url: "https://cosmwasm.com/",
-	},
-	{
-		title: "Cartesi",
-		image: "ecosystem/cartesi.png",
-		url: "https://cartesi.io/",
-	},
-	{
-		title: "Axelar",
-		image: "ecosystem/axelar.png",
-		url: "https://axelar.network/",
-	},
-	{
-		title: "Hyperlane",
-		image: "ecosystem/hyperlane.png",
-		url: "https://www.hyperlane.xyz/",
-	},
-	{
-		title: "Orb",
-		image: "ecosystem/orb.png",
-		url: "https://www.orblabs.xyz/",
-	},
-	{
-		title: "Squid",
-		image: "ecosystem/squid.png",
-		url: "https://www.squidrouter.com/",
-	},
-	{
-		title: "Skip",
-		image: "ecosystem/skip.png",
-		url: "https://skip.money/",
-	},
-	{
-		title: "Cosmostation",
-		image: "ecosystem/cosmostation.png",
-		url: "https://www.cosmostation.io/",
-	},
-	{
-		title: "Keplr",
-		image: "ecosystem/keplr.png",
-		url: "https://www.keplr.app/",
-	},
-	{
-		title: "Leap wallet",
-		image: "ecosystem/leap-wallet.png",
-		url: "https://www.leapwallet.io/",
-	},
-	{
-		title: "Catalyst",
-		image: "ecosystem/catalyst.png",
-		url: "https://catalyst.exchange/",
-	},
-	{
-		title: "Shogun",
-		image: "ecosystem/shogun.png",
-		url: "https://twitter.com/shogunfi/",
-	},
-	{
-		title: "Osmosis",
-		image: "ecosystem/Osmosis.png",
-		url: "https://osmosis.zone/",
-	},
-	{
-		title: "Argus",
-		image: "ecosystem/argus.png",
-		url: "https://argus.gg/",
-	},
-	{
-		title: "Curio",
-		image: "ecosystem/curio.png",
-		url: "https://www.curio.gg/",
-	},
-	{
-		title: "Worlds",
-		image: "ecosystem/worlds.png",
-		url: "https://www.worlds.org/",
-	},
-	{
-		title: "Bullieverse",
-		image: "ecosystem/bullieverse.png",
-		url: "https://bullieverse.com/",
-	},
-	{
-		title: "Aether Games",
-		image: "ecosystem/aethergames.png",
-		url: "https://www.aethergames.io/",
-	},
-	{
-		title: "PK Lab",
-		image: "ecosystem/pklab.png",
-		url: "https://bakingbad.dev/",
+		title: "LightLink",
+		image: "ecosystem/lightlink.png",
+		url: "https://lightlink.io/",
 	},
 	{
 		title: "Manta Network",
 		image: "ecosystem/manta.png",
 		url: "https://manta.network/",
 	},
-	{
-		title: "Modular cloud",
-		image: "ecosystem/modular-cloud.png",
-		url: "https://explorer.modular.cloud/",
-	},
-	{
-		title: "Range",
-		image: "ecosystem/range.png",
-		url: "https://www.range.org/",
-	},
-	{
-		title: "Noble",
-		image: "ecosystem/noble.png",
-		url: "https://nobleassets.xyz/",
-	},
-	{
-		title: "Risc Zero",
-		image: "ecosystem/risczero.png",
-		url: "https://www.risczero.com/",
-	},
-	{
-		title: "Dora",
-		image: "ecosystem/dora.png",
-		url: "https://www.ondora.xyz/",
-	},
-	{
-		title: "Cosmology",
-		image: "ecosystem/cosmology.png",
-		url: "https://cosmology.tech/",
-	},
-	{
-		title: "Numia",
-		image: "ecosystem/numia.png",
-		url: "https://www.numia.xyz/",
-	},
+			{
+				title: "AsMatch",
+				image: "ecosystem/asmatch.png",
+				url: "https://www.asmatch.app/",
+			},
+			{
+				title: "Movement Labs",
+				image: "ecosystem/movement.png",
+				url: "https://movementlabs.xyz/",
+			},
+			{
+				title: "Ojo",
+				image: "ecosystem/ojo.png",
+				url: "https://ojo.network/",
+			},
+			{
+				title: "Deri",
+				image: "ecosystem/deri.png",
+				url: "https://deri.io/#/index",
+			},
+			{
+				title: "Paima Studios",
+				image: "ecosystem/paima.png",
+				url: "https://paimastudios.com/",
+			},
+			{
+				title: "Curio",
+				image: "ecosystem/curio.png",
+				url: "https://www.curio.gg/",
+			},
+			{
+				title: "Pocket Network",
+				image: "ecosystem/pocket.png",
+				url: "https://www.pokt.network/",
+			},
+			{
+				title: "Shogun",
+				image: "ecosystem/shogun.png",
+				url: "https://twitter.com/shogunfi/",
+			},
+			{
+				title: "Eclipse",
+				image: "ecosystem/eclipse.png",
+				url: "https://www.eclipse.builders/",
+			},
+			{
+				title: "Syndr",
+				category: ["Rollup"],
+				image: "ecosystem/syndr.png",
+				url: "https://www.syndr.com/",
+			},
+			{
+				title: "Hokum",
+				image: "ecosystem/hokum.png",
+				url: "https://hokum.gg/",
+			},
+			{
+				title: "UniDex",
+				image: "ecosystem/unidex.png",
+				url: "https://www.unidex.exchange/",
+			},
+			{
+				title: "Kinto",
+				image: "ecosystem/kinto.png",
+				url: "https://kinto.xyz/",
+			},
+			{
+				title: "Waev",
+				image: "ecosystem/waev.png",
+				url: "https://www.waev.com/",
+			},
+			{
+				title: "Zeko",
+				image: "ecosystem/zeko.png",
+				url: "https://zeko.io/",
+			},
+			{
+				title: "ZKFair",
+				image: "ecosystem/zkfair.png",
+				url: "https://zkfair.io/",
+			},
+			{
+				title: "Last L2",
+				image: "ecosystem/lastl2.png",
+				url: "https://golast.xyz/",
+			},
+			{
+				title: "Dark Frontier",
+				image: "ecosystem/darkfrontier.png",
+				url: "https://darkfrontier.gg/",
+			},
+			{
+				title: "Forma",
+				image: "ecosystem/forma.png",
+				url: "",
+			},
+			{
+				title: "Caldera",
+				image: "ecosystem/caldera.png",
+				url: "https://www.caldera.xyz/",
+			},
+			{
+				title: "Vistara",
+				image: "ecosystem/vistara.png",
+				url: "https://docs.vistara.dev/",
+			},
+			{
+				title: "Snapchain",
+				image: "ecosystem/snapchain.png",
+				url: "https://www.snapchain.dev/",
+			},
+			{
+				title: "AltLayer",
+				image: "ecosystem/altlayer.png",
+				url: "https://www.altlayer.io/",
+			},
+			{
+				title: "Conduit",
+				image: "ecosystem/conduit.png",
+				url: "https://conduit.xyz/",
+			},
+			{
+				title: "Gateway",
+				image: "ecosystem/gateway.png",
+				url: "https://gateway.fm/",
+			},
+			{
+				title: "Gelato",
+				image: "ecosystem/gelato.png",
+				url: "https://www.gelato.network/",
+			},
+			{
+				title: "Lumoz",
+				image: "ecosystem/lumoz.png",
+				url: "https://lumoz.org/",
+			},
+			{
+				title: "Zeeve",
+				image: "ecosystem/zeeve.png",
+				url: "https://www.zeeve.io/",
+			},
+			{
+				title: "Karnot",
+				image: "ecosystem/karnot.png",
+				url: "https://www.karnot.xyz/",
+			},
+			{
+				title: "Rollkit",
+				image: "ecosystem/rollkit.png",
+				url: "https://www.rollkit.dev/",
+			},
+			{
+				title: "Sovereign SDK",
+				image: "ecosystem/sovereign.png",
+				url: "https://sovereign.xyz/",
+			},
+			{
+				title: "OP Stack",
+				image: "ecosystem/optimism.png",
+				url: "https://docs.optimism.io/stack/getting-started",
+			},
+			{
+				title: "Stackr",
+				image: "ecosystem/stackr.png",
+				url: "https://www.stackrlabs.xyz/",
+			},
+			{
+				title: "Arbitrum Orbit",
+				image: "ecosystem/arbitrum.png",
+				url: "https://arbitrum.io/orbit",
+			},
+			{
+				title: "Dymint",
+				image: "ecosystem/dymension.png",
+				url: "https://dymension.xyz/",
+			},
+			{
+				title: "Polygon CDK",
+				image: "ecosystem/polygoncdk.png",
+				url: "https://polygon.technology/polygon-cdk",
+			},
+			{
+				title: "Cartesi",
+				image: "ecosystem/cartesi.png",
+				url: "https://cartesi.io/",
+			},
+			{
+				title: "Keystone",
+				image: "ecosystem/curio.png",
+				url: "https://www.keystonesdk.com/",
+			},
+			{
+				title: "Radius",
+				image: "ecosystem/radius.png",
+				url: "https://www.theradius.xyz/",
+			},
+			{
+				title: "Fairblock",
+				image: "ecosystem/fairblock.png",
+				url: "https://twitter.com/Fair_Block",
+			},
+			{
+				title: "Madara",
+				image: "ecosystem/madara.png",
+				url: "https://www.madara.zone/",
+			},
+			{
+				title: "NodeKit",
+				image: "ecosystem/nodekit.png",
+				url: "https://www.nodekit.xyz/",
+			},
+			{
+				title: "Neutron",
+				image: "ecosystem/neutron.png",
+				url: "https://neutron.org/",
+			},
+			{
+				title: "Berachain",
+				image: "ecosystem/berachain.png",
+				url: "https://berachain.com/",
+			},
+			{
+				title: "Saga",
+				image: "ecosystem/saga.png",
+				url: "https://www.saga.xyz/",
+			},
+			{
+				title: "Dymension",
+				image: "ecosystem/dymension.png",
+				url: "https://dymension.xyz/",
+			},
+			{
+				title: "Osmosis",
+				image: "ecosystem/osmosis.png",
+				url: "https://osmosis.zone/",
+			},
+			{
+				title: "Initia",
+				image: "ecosystem/initia.png",
+				url: "https://twitter.com/initiaFDN",
+			},
+			{
+				title: "Argus",
+				image: "ecosystem/argus.png",
+				url: "https://argus.gg/",
+			},
+			{
+				title: "Axelar",
+				image: "ecosystem/axelar.png",
+				url: "https://axelar.network/",
+			},
+			{
+				title: "Hyperlane",
+				image: "ecosystem/hyperlane.png",
+				url: "https://www.hyperlane.xyz/",
+			},
+			{
+				title: "Catalyst",
+				image: "ecosystem/catalyst.png",
+				url: "https://catalyst.exchange/",
+			},
+			{
+				title: "Squid",
+				image: "ecosystem/squid.png",
+				url: "https://www.squidrouter.com/",
+			},
+			{
+				title: "Skip",
+				image: "ecosystem/skip.png",
+				url: "https://skip.money/",
+			},
+			{
+				title: "Mitosis",
+				image: "ecosystem/mitosis.png",
+				url: "https://twitter.com/MitosisOrg",
+			},
+			{
+				title: "Polyhedra",
+				image: "ecosystem/polyhedra.png",
+				url: "https://polyhedra.network/",
+			},
+			{
+				title: "Wormhole",
+				image: "ecosystem/wormhole.png",
+				url: "https://wormhole.com/",
+			},
+			{
+				title: "Union",
+				image: "ecosystem/union.png",
+				url: "https://union.build/",
+			},
+			{
+				title: "Orb Labs",
+				image: "ecosystem/orb.png",
+				url: "https://www.orblabs.xyz/",
+			},
+			{
+				title: "Celenium",
+				image: "ecosystem/celenium.png",
+				url: "https://celenium.io/",
+			},
+			{
+				title: "Dora",
+				image: "ecosystem/dora.png",
+				url: "https://www.ondora.xyz/",
+			},
+			{
+				title: "Mintscan",
+				image: "ecosystem/mintscan.png",
+				url: "https://www.mintscan.io/celestia",
+			},
+			{
+				title: "Modular Cloud",
+				image: "ecosystem/modularcloud.png",
+				url: "https://explorer.modular.cloud/celestia-mainnet",
+			},
+			{
+				title: "Numia",
+				image: "ecosystem/numia.png",
+				url: "https://www.numia.xyz/",
+			},
+			{
+				title: "Range",
+				image: "ecosystem/range.png",
+				url: "https://www.range.org/",
+			},
+			{
+				title: "Cosmostation",
+				image: "ecosystem/cosmostation.png",
+				url: "https://www.cosmostation.io/",
+			},
+			{
+				title: "Keplr",
+				image: "ecosystem/keplr.png",
+				url: "https://www.keplr.app/",
+			},
+			{
+				title: "Leap wallet",
+				image: "ecosystem/leap-wallet.png",
+				url: "https://www.leapwallet.io/",
+			},
+			{
+				title: "Cosmology",
+				image: "ecosystem/cosmology.png",
+				url: "https://cosmology.tech/",
+			},
+			{
+				title: "Aori",
+				image: "ecosystem/aori.png",
+				url: "https://www.aori.io/",
+			},
+			{
+				title: "Noble",
+				image: "ecosystem/noble.png",
+				url: "https://nobleassets.xyz/",
+			},
+			{
+				title: "Risc Zero",
+				image: "ecosystem/risczero.png",
+				url: "https://www.risczero.com/",
+			},
+			{
+				title: "Marlin",
+				image: "ecosystem/marlin.png",
+				url: "https://www.marlin.org/",
+			},
+			{
+				title: "Nebra",
+				category: ["Infrastructure"],
+				image: "ecosystem/nebra.png",
+				url: "https://www.nebra.one/",
+			},
+			{
+				title: "Redstone",
+				image: "ecosystem/redstone.png",
+				url: "https://redstone.finance/",
+			},
+			{
+				title: "Privy",
+				image: "ecosystem/privy.png",
+				url: "https://www.privy.io/",
+			},
+			{
+				title: "ZeroDev",
+				image: "ecosystem/zerodev.png",
+				url: "https://zerodev.app/",
+			},
+			{
+				title: "Arc",
+				image: "ecosystem/arc.png",
+				url: "https://www.onarc.io/",
+			},
+			{
+				title: "Polaris",
+				image: "ecosystem/berachain.png",
+				url: "https://berachain.com/",
+			},
+			{
+				title: "CosmWasm",
+				image: "ecosystem/cosmwasm.png",
+				url: "https://cosmwasm.com/",
+			},
+	
 ];
 
 // 2. Define ecosystem categories and assign ecosystem items to them.
@@ -263,44 +431,48 @@ export const ecosystemCategories = {
 	order: "none", // specify the vertical order of categories on the categories panel:  asc | desc | none  (default: none - will render items in order as you define them in the categories array)
 	categories: [
 		{
-			name: "Rollup framework",
-			items: ["Rollkit", "Sovereign", "OP Stack", "Stackr", "Cosmos SDK", "Arbitrum Orbit", "Dymension", "Polygon CDK"],
+			name: "Rollups",
+			items: ["Alpha Dune", "AsMatch", "Astria", "Curio", "Dark Frontier", "Deri", "Eclipse", "Forma", "Hokum", "Kinto", "Last L2", LightLink", "Manta Network", "Movement Labs", "Ojo", "Paima Studios", "Pocket Network", "Shogun", "Syndr", "UniDex", "Waev", "Zeko", "ZKFair"],
 		},
 		{
 			name: "Rollups-as-a-service",
-			items: ["Rollkit", "Caldera", "Vistara", "Snapchain", "Altlayer", "Astria", "Conduit", "Gateway", "Gelato", "Lumoz", "Zeeve"],
+			items: ["AltLayer", "Astria", "Caldera", "Conduit", "Gateway", "Gelato", "Karnot", "Lumoz", "Snapchain", "Vistara", "Zeeve"],
 		},
 		{
-			name: "Shared sequencer network",
-			items: ["Radius", "Fairblock"],
+			name: "Shared sequencer",
+			items: ["Astria", "Fairblock", "Madara", "NodeKit", "Radius"],
 		},
 		{
-			name: "Settlement layer",
-			items: ["Neutron", "Berachain", "Saga"],
+			name: "Rollup framework",
+			items: ["Arbitrum Orbit", "Cartesi", "Dymint", "Keystone", "OP Stack", "Polygon CDK", "Rollkit", "Sovereign SDK", "Stackr"],
 		},
 		{
 			name: "Virtual machine",
 			items: ["Polaris", "CosmWasm", "Cartesi"],
 		},
 		{
+			name: "Settlement layer & bridging hub",
+			items: ["Neutron", "Berachain", "Saga", "Argus", "Dymension", "Initia", "Osmosis"],
+		},
+		{
 			name: "Cross chain",
-			items: ["Axelar", "Hyperlane", "Orb", "Squid", "Skip"],
+			items: ["Axelar", "Catalyst", "Hyperlane", "Mitosis", "Orb Labs", "Polyhedra", "Skip", "Squid", "Union", "Wormhole"],
+		},
+		{
+			name: "Explorer",
+			items: ["Celenium", "Dora", "Mintscan", "Modular Cloud"],
+		},
+		{
+			name: "Analytics",
+			items: ["Numia", "Range"],
 		},
 		{
 			name: "Wallet",
-			items: ["Cosmostation", "Keplr", "Leap wallet"],
-		},
-		{
-			name: "DeFi",
-			items: ["Catalyst", "Shogun", "Osmosis"],
-		},
-		{
-			name: "Gaming",
-			items: ["Argus", "Curio", "Worlds", "Bullieverse", "Aether Games"],
+			items: ["Cosmostation", "Keplr", "Leap wallet", "Cosmology"],
 		},
 		{
 			name: "Infrastructure",
-			items: ["PK Lab", "Manta Network", "Modular cloud", "Range", "Noble", "Risc Zero", "Dora", "Cosmology", "Numia"],
+			items: ["Aori", "Arc", "Marlin", "Nebra", "Noble", "Privy", "Redstone", "Risc Zero", "ZeroDev"],
 		},
 	],
 };
