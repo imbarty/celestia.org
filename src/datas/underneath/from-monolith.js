ae2f96211d450c16a0ddc1cb7e1192f82c0db1a5
Alex Beckett <75361908+alex-beckett@users.noreply.github.com>
2024-06-18 18:41:57 +0930

    Update from-monolith.js

--- src/datas/underneath/from-monolith.js
+++ src/datas/underneath/from-monolith.js
@@ -1,9 +1,9 @@
 export const fromMonolith = {
 	title: "Monolithic to modular",
 	text: `
-    <p>Bitcoin was the first blockchain and decentralized app. But Bitcoin’s limited functionality couldn’t support new applications.</p>
-    <p>Then Ethereum built the first blockchain you could program for apps. However, developers faced scaling issues and limited customizability in Ethereum’s monolithic architecture.</p>
-    <p>Now, Celestia introduces a new modular architecture that makes it easy for anyone to securely launch their own blockchain in just one click. With Celestia underneath, developers get scale and customizability to build new apps.</p>
+    <p>Bitcoin was the first blockchain and decentralized app. But Bitcoin wasn’t made for others to build apps on, so developers began building their own basic L1s.</p>
+    <p>Then Ethereum built the first blockchain you could program for apps. However, developers continued launching L1s with increasing development times and costs.</p>
+    <p>Now, Celestia introduces a new modular architecture that makes it easy for anyone to securely launch their own blockchain. Without needing to recruit a committee, you can deploy your own chain with Celestia underneath in just one click.</p>
     `,
 	image: {
 		src: "underneath/celestia-underneath-from-monolith.png",
